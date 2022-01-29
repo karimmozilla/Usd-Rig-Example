85a823397bd88c0a78836c7225a0715f9007cd52
karimmozlia <karimmozilla@gmail.com>
2022-01-30 01:17:29 +0200

    missed docstring

--- modules/os/lib.py
+++ modules/os/lib.py
@@ -2,4 +2,13 @@ import os
 
 
 def _fr(path):
+    """
+    Convert Back Slashes to forward slashes
+
+    Args:
+        path (str): system path
+
+    Returns:
+        str: system path converted
+    """
     return path.replace(os.sep, "/")
